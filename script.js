1cb4dbf027879d5d7bc48665c1374def17a46bbb
sharma-shivam21 <s3860762@gmail.com>
2025-05-21 10:57:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function shortcut(s1, s2) {
-  // your code here
+  if (!s1 || !s2) {
+    return '';
+  }
+  return s1[0] + s2[0];
 }
 
+// Test cases
+console.log(shortcut("Amnesty", "International")); // 'AI'
+console.log(shortcut("Hello", "world"));           // 'Hw'
+console.log(shortcut("", "International"));        // ''
+console.log(shortcut("Amnesty", ""));              // ''
+
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+const s1Input = prompt("Enter s1:");
+const s2Input = prompt("Enter s2:");
+alert(shortcut(s1Input, s2Input));
